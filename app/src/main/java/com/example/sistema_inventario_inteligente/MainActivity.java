503f125b422dab13aeebf161cf03fe0e77b8202f
JColocho <Jcolocho2003@gmail.com>
2026-05-21 04:02:05 -0600

    refactor: Eliminar código muerto

--- app/src/main/java/com/example/sistema_inventario_inteligente/MainActivity.java
+++ app/src/main/java/com/example/sistema_inventario_inteligente/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.sistema_inventario_inteligente;
 
 import android.os.Bundle;
 import android.widget.FrameLayout;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,10 +10,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
 
-import com.example.sistema_inventario_inteligente.models.Categoria;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     private BottomNavigationView navigationView;
@@ -31,59 +27,27 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-
-        /*
-        //Conexion a la base de datos
-        //Obtener la instancia a la base de datos
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-
-        //Crear la referencia a la tabla
-        DatabaseReference categoriaRef = database.getReference("Categorias");
-        //Crear el objeto con todos los datos a guardar
-        Categoria categoria = new Categoria("Controles");
-
-        //.push() crea un ID único aleatorio (ej. -NjsdH83jd9) para que no se dupliquen
-        // y .setValue() inserta el objeto allí.
-        String idCategoria = categoriaRef.push().getKey();
-
-        categoria.setIdCategoria(idCategoria);
-
-        categoriaRef.child(idCategoria).setValue(categoria);
-        */
-
-
-
-
-
         navigationView = findViewById(R.id.navegationView);
         frameVista = findViewById(R.id.containerMain);
 
         loadFragment(new InicioFragment());
 
-        //Manejo de fragment
         navigationView.setOnItemSelectedListener(item -> {
-            if(item.getItemId() == R.id.inicio){
+            if (item.getItemId() == R.id.inicio) {
                 loadFragment(new InicioFragment());
                 return true;
-            }
-            else if(item.getItemId() == R.id.invetario){
+            } else if (item.getItemId() == R.id.invetario) {
                 loadFragment(new InventarioFragment());
                 return true;
-
-
             } else if (item.getItemId() == R.id.sucursal) {
                 loadFragment(new SucursalFragment());
                 return true;
             }
-
             return false;
         });
-
-
-
     }
-    //Metodo para mostrar vista seleccionada
-    private void loadFragment(Fragment fragment){
+
+    private void loadFragment(Fragment fragment) {
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.containerMain, fragment)
